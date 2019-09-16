1d49d36946658a4f30917c893f63b37a921abc1a
Matthieu Bizien <matthieu@databiz.io>
2019-09-16 13:57:04 +0200

    fix

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ set -e
 
 dirname=$1
 # aws s3 sync --delete s3://yokai-data/dev/pytorch-build/$dirname/build ./build_/
-cp -r ~/Programmes/pytorch-build/$dirname/build build_
-docker build . -t test_patch_pytorch
+rm -r build_ || true
+mkdir -p build_/s
+cp -r ~/Programmes/pytorch-build/$dirname/build/lib build_/lib
+docker build . -t test_patch_pytorch:$dirname
